70cc71c64d973b7fa79a1d66429a7878b5aaae87
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-02-09 21:00:49 -0700

    confused on server? or postman

--- api/server.js
+++ api/server.js
@@ -6,6 +6,11 @@ server.use(express.json());
 
 console.log("server is working")
 
+server.get("/api/users", async (req, res) => {
+    const {name , bio} = req.body
+    dbFunctions.find(name, bio)
+    res.status(200).json(name, bio)
+})
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
