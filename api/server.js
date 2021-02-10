b423cc0dff964911b0f438adc129fe335c695bde
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-02-09 20:26:34 -0700

    server is up

--- api/server.js
+++ api/server.js
@@ -1,3 +1,11 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const server = express();
+const dbFunctions = require('./users/model')
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+console.log("server is working")
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
